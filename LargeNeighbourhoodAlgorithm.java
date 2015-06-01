cb691b10f89767d355a0ddd416a06de1e18ae23a
Grakus <grakus1993@tlen.com>
2015-06-01 13:01:30 +0200

    Solmon's Insertion Best Lambda Choose

--- LargeNeighbourhoodAlgorithm.java
+++ LargeNeighbourhoodAlgorithm.java
@@ -7,13 +7,23 @@ public class LargeNeighbourhoodAlgorithm implements IAlgorithm {
 
     @Override
     public Solution run(Configuration configuration) {
-        double lambdaParameter = 1.0;
-        Solution solution = new SolomonInsertion().getInitialSolution(configuration, lambdaParameter);
-        Solution bestSolution = solution;
-
-
-
-        return solution;
+        Solution bestSolution = new SolomonInsertion().getInitialSolution(configuration, 0.9);
+        double bestCost = bestSolution.getCost();
+        //double bestLambda = 0.0;
+        //System.out.println("Lambda: " + 0.0 + " Cost: " + bestCost);
+        for(double i = 0.91; i <= 1.1 ; i=i+0.01)
+        {
+            Solution solution = new SolomonInsertion().getInitialSolution(configuration, i);
+            double cost = solution.getCost();
+            if (cost < bestCost) {
+                bestSolution = solution;
+                bestCost = cost;
+                //bestLambda = i;
+            }
+            //System.out.println("Lambda: " + i + " Cost: " + cost);
+        }
+        //System.out.println("The best lambda is:" + bestLambda);
+        return bestSolution;
     }
 
 }
